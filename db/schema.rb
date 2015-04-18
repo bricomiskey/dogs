b0493ee57ac33c3c2c2b0f210e7c92b20ceec015
Bri Comiskey <bricomiskey@gmail.com>
2015-04-18 16:20:38 -0500

    added pictures to dogs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418201830) do
+ActiveRecord::Schema.define(version: 20150418211804) do
 
   create_table "dogs", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150418201830) do
     t.text     "color"
     t.text     "temperament"
     t.text     "fixed"
+    t.string   "picture"
   end
 
 end
